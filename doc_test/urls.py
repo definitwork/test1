06a4ea7869d3d6d6964dafda8b5fa5fc16a53f0e
Igor Kartel <igor.v.kartel@gmail.com>
2023-12-13 20:28:07 +0300

    correction of static

--- doc_test/urls.py
+++ doc_test/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from .views import get_page
@@ -5,3 +7,6 @@ from .views import get_page
 urlpatterns = [
     path('', get_page),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
