26000b900f90ff24ac5ae8031a305ebab05ca181
Ko100fei <dobysh.k@gmail.com>
2023-12-13 13:29:58 +0300

    create urls.py and endpoint doc/

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('test1_app.urls')),
+    path('doc/', include('doc_test.urls')),
 ]
